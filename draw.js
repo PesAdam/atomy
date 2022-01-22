83f0fdc7f3bd8e2b1a4cb849d90d898a10890ee4
PesAdam <pesadam15@gmail.com>
2022-01-22 17:13:39 +0100

    pokusanie sa o kreslenie s canvas elementom

--- draw.js
+++ draw.js
@@ -1,45 +1,56 @@
-var Draw = {};
+    var Draw = {
+    CELL: 60,
+    LINE: 2,
+    ATOM: 7,
+    _context: null
+};
+/* nastavenie canvas */
+Draw.init = function(){
+    var canvas = document.createElement("canvas");
 
-Draw.all = function (){
-    var html = "<table>";
-    for (var i =0; i<Board.length; i++){
-        html += "<tr>";
-        for (var j = 0; j < Board[i]; j++){
-            html += "<td>";
-            html += Draw.atoms(Board[j][i]);
-            html += "</td>";
-        }
-        html += "</tr>";
-    }
-    html += "</table>";
+    this.CELL += this.LINE;
 
-    document.body.innerHTML = html;
+    var size = Game.size * this.CELL + this.LINE;
+    canvas.width = size;
+    canvas.height = size;
+
+    this._context = canvas.getContext("2d");
+    tihs._context.lineWidth = this.LINE;
+    
+    document.body.appendChild(canvas);
+
+    this.all();
 }
 
-Draw.atoms = function (count){
-    var result = "";
 
-    for(var i=0; i<count; i++){
-        result += "o";
-    }
+/* vykreslime celu hraciu plochu */
+Draw.all = function(){
+    this._context.fillStyle = "#fff";
+    var width = this._context.canvas.width;
+    var height = this._context.canvas.height;
 
-    return result;
+    this._context.fillReact(0,0, width, height);
+
+    this._lines();
+    this._cells();
 }
 
-Draw.getPosition = function (node){
-    if(node.nodeName != "TD") {return null; }
-    var x = 0;
-    while(node.previousSibling){
-        x++;
-        node = node.previousSibling;
+/*vykreslime mriezku */
+Draw._lines = function(){
+    this._context.beginPath();
+
+    for (var i=0; i < Game.SIZE+1; i++){ //zvisle
+        var x = this.LINE/2 + i*this.CELL;
+        this._context.moveTo(x, 0);
+        this._context.lineTo(x, this._context.canvas.height);
     }
 
-    var row = node.parentNode;
-    var y = 0;
-    while(row.previousSibling){
-        y++;
-        row = row.previousSibling;
+    for(var i=0; i < Game.SIZE+1; i++){ //vodorovne
+        var y = this.LINE/2 + i*this.CELL;
+        this._context.moveTo(0, y);
+        this._context.lineTo(this._context.canvas.width, y);
     }
 
-    return [x,y];
+    this._context.stroke();
 }
+
